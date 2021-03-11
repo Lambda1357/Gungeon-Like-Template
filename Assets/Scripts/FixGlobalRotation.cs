9d81831d2664d6292f2ddc51aa8f9651ce2d36c2
Bae Junhyeong <bjh6681@outlook.kr>
2021-03-11 23:18:10 +0900

    Import Project

---
+++ Assets/Scripts/FixGlobalRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixGlobalRotation : MonoBehaviour
+{
+    public Vector3 desiredGlobalRotationEuler = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = desiredGlobalRotationEuler;
+    }
+}
---
+++ Assets/Scripts/FollowTargetAgent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class FollowTargetAgent : MonoBehaviour
+{
+    public Transform target;
+
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target)
+            agent.destination = target.position;
+    }
+}
